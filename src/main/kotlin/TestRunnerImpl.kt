166d311892ce9e45e577cdd4201d47eaf06e094c
tsuleymanov <smatrix2005@yandex.ru>
2022-11-16 21:38:51 +0700

    hw01-testrunner

--- src/main/kotlin/TestRunnerImpl.kt
+++ src/main/kotlin/TestRunnerImpl.kt
@@ -1,13 +1,13 @@
 import kotlin.reflect.full.declaredMemberFunctions
 
-class TestRunnerImpl: TestRunner {
+class TestRunnerImpl : TestRunner {
 
     override fun <T> runTest(steps: T, test: () -> Unit) {
         val before = steps!!::class.declaredMemberFunctions.filter { it.name.contains("before") }
         val after = steps!!::class.declaredMemberFunctions.filter { it.name.contains("after") }
-        before.forEach { x -> x.call(steps) }
+        before.forEach { x -> x.call(steps).apply { println("method ${x.name}() called") } }
         steps.apply { test() }
-        after.reversed().forEach { x -> x.call(steps) }
+        after.reversed().forEach { x -> x.call(steps).apply { println("method ${x.name}() called") } }
     }
 
 }
