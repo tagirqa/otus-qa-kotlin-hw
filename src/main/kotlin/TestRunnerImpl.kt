3360d41161eb0a5ab153f0b6ceaae4398788d734
tsuleymanov <smatrix2005@yandex.ru>
2022-11-18 13:18:37 +0700

    added hyphens

--- src/main/kotlin/TestRunnerImpl.kt
+++ src/main/kotlin/TestRunnerImpl.kt
@@ -6,11 +6,28 @@ class TestRunnerImpl : TestRunner {
     private val logger = KotlinLogging.logger {}
 
     override fun <T> runTest(steps: T, test: () -> Unit) {
-        val before = steps!!::class.declaredMemberFunctions.filter { it.name.contains("before") }
-        val after = steps!!::class.declaredMemberFunctions.filter { it.name.contains("after") }
-        before.forEach { x -> x.call(steps).apply { logger.info("method ${x.name}() called") } }
-        steps.apply { test() }
-        after.reversed().forEach { x -> x.call(steps).apply { logger.info("method ${x.name}() called") } }
+        val before = steps!!::class.declaredMemberFunctions.filter {
+            it.name.contains("before")
+        }
+        val after = steps!!::class.declaredMemberFunctions.filter {
+            it.name.contains("after")
+        }
+
+        before.forEach { x ->
+            x.call(steps).apply {
+                logger.info("method ${x.name}() called")
+            }
+        }
+
+        steps.apply {
+            test()
+        }
+
+        after.reversed().forEach { x ->
+            x.call(steps).apply {
+                logger.info("method ${x.name}() called")
+            }
+        }
     }
 
 }
